82e7d79214163336bd1cf2142ac9a235add00300
Ashley Philip <ashleyep@umich.edu>
2023-02-04 17:55:02 -0500

    Bella's updated scene + some script changes to detecting sound

--- Assets/Scenes/Soundscripts/DetectRightNote.cs
+++ Assets/Scenes/Soundscripts/DetectRightNote.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class DetectRightNote : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//    public AudioSource[] soundFX;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("akey"))
+        {
+            // soundFX[0].Play();
+            
+            
+        }  
+        if (Input.GetButtonDown("skey"))
+        {
+            // soundFX[1].Play();
+            
+        } 
+        if (Input.GetButtonDown("dkey"))
+        {
+            // soundFX[2].Play();
+            
+        } 
+        if (Input.GetButtonDown("fkey"))
+        {
+            // soundFX[3].Play();
+            
+        } 
+        if (Input.GetButtonDown("jkey"))
+        {
+            // soundFX[4].Play();
+            
+        }  
+        if (Input.GetButtonDown("kkey"))
+        {
+            // soundFX[5].Play();
+            
+        } 
+        if (Input.GetButtonDown("lkey"))
+        {
+            // soundFX[6].Play();
+            
+        } 
+        if (Input.GetButtonDown(";key"))
+        {
+            // soundFX[7].Play();
+            
+        } 
+        if (Input.GetButtonDown("spaceKey"))
+        {
+            // soundFX[8].Play();
+            
+        } 
     }
+
+    // Update is called once per frame
 }

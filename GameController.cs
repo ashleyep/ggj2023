0db8e1dddaf40f4c629c451b80e44fcd769c54e6
cbpalumbi <christabellapalumbi@gmail.com>
2023-02-04 17:22:39 -0500

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    
+    public float destinationY = -208f;
+    public float speed = 50f;
+    public RectTransform[] panels;
+
+
+    private RectTransform topPanel;
+    private RectTransform middlePanel;
+    private Vector3 startPosTop;
+    private Vector3 startPosMiddle;
+    private Vector3 topOriginalPos;
+    private Vector3 middleOriginalPos;
+    private int panelCounter = 0;
+
+    
+    void Start() {
+        startPosTop = new Vector3(0, 430);
+        startPosMiddle = new Vector3(0, 0);
+
+        topPanel = panels[panelCounter];
+        topOriginalPos = topPanel.anchoredPosition;
+        topPanel.anchoredPosition = startPosTop;
+
+        panelCounter++;
+        middlePanel = panels[panelCounter];
+        middleOriginalPos = middlePanel.anchoredPosition;
+        middlePanel.anchoredPosition = startPosMiddle;
+    }
+    
+    void Update()
+    {
+        var step =  speed * Time.deltaTime;
+        if (topPanel != null) {
+            topPanel.anchoredPosition = Vector2.MoveTowards(topPanel.anchoredPosition, new Vector2(topPanel.anchoredPosition.x, destinationY), step);
+            if (topPanel.anchoredPosition.y == destinationY) {
+                ResetTopPanel();
+            }
+        }
+        if (middlePanel != null) {
+            middlePanel.anchoredPosition = Vector2.MoveTowards(middlePanel.anchoredPosition, new Vector2(middlePanel.anchoredPosition.x, destinationY), step);
+            if (middlePanel.anchoredPosition.y == destinationY) {
+                ResetMiddlePanel();
+            }
+        }
+    }
+
+    void ResetTopPanel() {
+        topPanel.anchoredPosition = topOriginalPos; // move back to original pos
+        panelCounter++; // increment counter
+        topPanel = panels[panelCounter % panels.Length]; // get next panel from list
+        topOriginalPos = topPanel.anchoredPosition; // save original pos
+        topPanel.anchoredPosition = startPosTop; // move to top of screen
+    }
+
+    void ResetMiddlePanel() {
+        middlePanel.anchoredPosition = middleOriginalPos; // move back to original pos
+        panelCounter++; // increment counter
+        middlePanel = panels[panelCounter % panels.Length]; // get next panel from list
+        middleOriginalPos = middlePanel.anchoredPosition; // save original pos
+        middlePanel.anchoredPosition = startPosTop; // move to top of screen
+    }
+}
